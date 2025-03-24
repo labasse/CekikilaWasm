c3de97c37c6dc9b482d8f1fffea1803f9ffd696f
Labasse <labasse@gmail.com>
2025-03-24 13:38:06 +0100

    Nouveautés .Net et C#

---
+++ CekikilaWasm/Models/Objet.cs
@@ -0,0 +1,16 @@
+﻿namespace CekikilaWasm.Models
+{
+    public class Objet
+    {
+        public required string Nom { 
+            get;
+            set;
+        }
+    }
+    public record Reservation(
+        Objet Objet, 
+        string Email, 
+        DateTime Debut, 
+        TimeSpan Duree
+    );
+}
