ca619b19f3c5d9c10e61af740e21a1025f1bef00
Labasse <labasse@gmail.com>
2025-03-25 10:13:28 +0100

    Fondements - Routage

--- CekikilaWasm/Models/DataService.cs
+++ CekikilaWasm/Models/DataService.cs
@@ -3,7 +3,7 @@
     public class DataService
     {
         private readonly Item[] _items = {
-            new() { Id=1, Name="Broyeur de végétaux"      , Tags = [ "Jardin", "Electrique" ], Description = "Broyeur <strong>électrique</strong> d'une puissance de 2800W et d'un diamètre de coupe maximum de 45 mm convient parfaitement à un usage régulier." },
+            new() { Id=1, Name="Broyeur de végétaux"      , Tags = [ "Jardin", "Electrique" ], Description = "Broyeur électrique d'une puissance de 2800W et d'un diamètre de coupe maximum de 45 mm convient parfaitement à un usage régulier." },
             new() { Id=4, Name="Paddle gonflable"         , Tags = [ "Sport" ], Description = "Idéal pour amener partout avec soit, les planches de paddle gonflable vous surprendront par leur rigidité une fois gonflée et leur compacité dans leur sac de rangement." },
             new() { Id=3, Name="Taille haies électrique"  , Tags = [ "Jardin", "Electrique" ], Description = "Taille haies filaire idéal pour tailler de grandes haies avec facilité et en silence." },
             new() { Id=6, Name="Combinaison néoprenne T40", Tags = [ "Sport" ], Description = "Cette combinaison vous permettra de nager toute l'année ! Conçue en néoprène spécial pour plus de résistance pour l'enfilage et une glisse idéale !" }
